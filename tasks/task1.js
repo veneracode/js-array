532e3fd57747c37bd41d5551bb4246641c9c7dc2
Ivan <tushchukivan@gmail.com>
2024-10-12 17:42:03 +0300

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -1,7 +1,16 @@
-//Функція filterAdults приймає масив об'єктів, що представлє людей (з полями name та age),
-//вам потрібно написати код, який повертає новий масив, який містить лише тих людей, чий вік більше або дорівнює 18.
 function filterAdults(people) {
-  //Ваш код
+  const adultUsers = people.filter(function(element) {
+    return element.age >= 18;
+  });
+  return adultUsers;
 }
 
-module.exports = filterAdults;
+const people = [
+  { name: "John", age: 21 },
+  { name: "Jane", age: 22 },
+  { name: "Jim", age: 19 },
+  { name: "Jill", age: 18 },
+  { name: "Jack", age: 23 },
+];
+
+console.log(filterAdults(people)); 
