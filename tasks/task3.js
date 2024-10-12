32fc7f4319ce1538c7b40f3b0bf2aadf9c269d95
Ivan <tushchukivan@gmail.com>
2024-10-12 17:42:12 +0300

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -1,6 +1,18 @@
 // З масива людей(people) Вам потрібно знайти людину за іменем(name)
 function findByName(people, name) {
-  // Ваш код
+  return people.find(function(item) {
+    return item.name === name;
+  });
 }
+
+const people = [
+  { name: "John", age: 20, score: 85 },
+  { name: "Jane", age: 22, score: 92 },
+  { name: "Jim", age: 19, score: 75 },
+  { name: "Jill", age: 21, score: 65 },
+  { name: "Jack", age: 23, score: 95 },
+];
+
+console.log(findByName(people, 'Jill')); 
 
 module.exports = findByName;
